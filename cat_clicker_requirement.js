2c82258b0824470fa1593787eeba5c841c40aa75
LalitKishor <lalitsinghbanna05@gmail.com>
2015-11-07 22:42:49 +0530

    cat clicker requirement 2

---
+++ cat_clicker_requirement.js
@@ -0,0 +1,11 @@
+var CLICK=1;
+$('#image1').click( function(){
+alert("the number of click first image "+ CLICK + " clicks");
+CLICK=CLICK+1;
+});
+var clk=1
+$('#image2').click( function(){
+alert("the number of click second image"+ clk + " clicks");
+clk=clk+1;
+});
+
